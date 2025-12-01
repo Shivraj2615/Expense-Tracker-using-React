3e0d161926ee706f45087d856ca4a34f82bb838d
Shivraj2615 <shivdilipjagdale@gmail.com>
2025-12-01 12:31:34 +0530

    Correction in Styling

--- src/ExpenseItem.jsx
+++ src/ExpenseItem.jsx
@@ -1,10 +1,17 @@
 export default function ExpenseItem({ txn, deleteTxn }) {
   return (
     <li
-      style={{ color: txn.expType === "Income" ? "green" : "red" }}
+      style={{
+        color: txn.expType === "Income" ? "green" : "red",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px 0",
+        borderBottom: "1px solid #eee",
+      }}
     >
       <h3>
-        {txn.description}: &#8377;{txn.amount}{" "}
+        {txn.description}: &#8377;{txn.amount}
       </h3>
       <h4>{txn.date}</h4>
       <button className="delete-btn" onClick={() => deleteTxn(txn.id)}>
